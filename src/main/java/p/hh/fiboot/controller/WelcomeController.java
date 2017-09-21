dd442a9cac655c026d3ee056b0eb1e24532128c4
haihan yin <haihan.yin@atos.net>
2017-09-21 09:47:51 +0200

    revert hateoas feature, find it difficult to design

--- src/main/java/p/hh/fiboot/controller/WelcomeController.java
+++ src/main/java/p/hh/fiboot/controller/WelcomeController.java
@@ -1,11 +1,5 @@
 package p.hh.fiboot.controller;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
-
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import p.hh.fiboot.dto.WelcomeDto;
@@ -14,9 +8,9 @@ import p.hh.fiboot.dto.WelcomeDto;
 public class WelcomeController {
 
     @RequestMapping("/welcome")
-    public HttpEntity<WelcomeDto> index() {
-        WelcomeDto wd = new WelcomeDto("ni hao!");
-        wd.add(linkTo(methodOn(WelcomeController.class).index()).withSelfRel());
-        return new ResponseEntity<WelcomeDto>(wd, HttpStatus.OK);
+    public WelcomeDto index() {
+        WelcomeDto wd = new WelcomeDto();
+        wd.setMessage("nihao");
+        return wd;
     }
 }
--- src/main/java/p/hh/fiboot/dto/WelcomeDto.java
+++ src/main/java/p/hh/fiboot/dto/WelcomeDto.java
@@ -1,16 +1,10 @@
 package p.hh.fiboot.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import org.springframework.hateoas.ResourceSupport;
-
-/**
- * Created by Atos on 20-9-2017.
- */
-public class WelcomeDto extends ResourceSupport {
+public class WelcomeDto {
 
     private String message;
 
-    public WelcomeDto(@JsonProperty("message") String message) {
+    public void setMessage(String message) {
         this.message = message;
     }
 
